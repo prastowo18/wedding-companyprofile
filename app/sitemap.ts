3100a5c96fc1c8dd7464f8b6703f43eb6648e7c6
prastowoadi18 <prastowoadiworkv@gmail.com>
2023-06-24 08:37:35 +0700

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 export default async function sitemap() {
-  const baseUrl = "https://wedding-companyprofile.vercel.app";
+  const baseUrl = "https://divory.vercel.app";
 
   return [{ url: baseUrl, lastModified: new Date() }];
 }
